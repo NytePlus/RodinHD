50bb9e3eb3c942d7b07852554ba88ab97eaac00f
NytePlus <nyte_plus@sjtu.edu.cn>
2024-11-08 17:02:10 +0000

    [code]save

--- Renderer/fit_stage1_dist.sh
+++ Renderer/fit_stage1_dist.sh
@@ -5,7 +5,7 @@ save_dir=/home/wcc/RodinHD/data/save_triplane_and_mlp_dist
 data_root=/home/wcc/RodinHD/data/raw_data_300
 fitting_obj_list=/home/wcc/RodinHD/data/fitting_obj_list_300.txt
 ckpt_dir=latest
-torchrun --nproc_per_node=3 --nnodes=1 --node_rank=0 main.py \
+mpirun -np 4 python main.py \
     ${fitting_obj_list} \
     ${save_dir} \
     --workspace ${save_dir} \
@@ -15,13 +15,12 @@ torchrun --nproc_per_node=3 --nnodes=1 --node_rank=0 main.py \
     --triplane_channels 32 \
     --ckpt ${ckpt_dir} \
     --data_root ${data_root} \
-    --out_loop_eps 30 --iters 5000 --lr0 2e-2 --lr1 2e-3 --eval_freq 1 \
+    --out_loop_eps 30 --iters 5000 --lr0 2e-3 --lr1 2e-4 --eval_freq 10 \
     --l1_reg_weight 1e-4 \
     --tv_weight 1e-2 \
     --dist_weight 0 \
     --iwc_weight 0.1 \
-    --num_rays -1 \
-    --num_steps 512 --upsample_steps 512 \
-    --ray_shuffle \
-    --max_ray_batch 1 \
+    --num_rays 4096 \
+    --max_steps 512 --downscale 2 \
+    --max_ray_batch 8192 \
     --update_extra_interval 1
